9fbaf40dd2a8639536c67c2902412166166a5fbf
Kisspeace <Kisspeace@github.com>
2022-12-20 09:56:56 -0800

    init

---
+++ test/download-pages.bat
@@ -0,0 +1,4 @@
+curl -fo feed.html "https://fapello.com/ajax/index/page-1/"
+curl -fo author_page.html "https://fapello.com/valeria-vixen/"
+curl -fo author_content.html "https://fapello.com/ajax/model/vinnegal/page-1/"
+curl -fo full.html "https://fapello.com/vinnegal/1836/"
